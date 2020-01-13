8c7fb0354666e686fb24b1838f6a4f67ba11c5e7
1irfanasi <32132552+1irfanasi@users.noreply.github.com>
2020-01-13 11:27:08 -0500

    Add files via upload

---
+++ deckofCards.java
@@ -0,0 +1,13 @@
+package simplecardexample;
+/**
+ *
+ * @author Asim
+ */
+public class deckofCards {
+    public static void main(String[]args){
+    card tenofHearts = new card("Heart", 150);
+    tenofHearts.setValue(200);
+    
+    }
+   
+}
